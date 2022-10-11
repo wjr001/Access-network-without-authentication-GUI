f2a2028c192447d2da001b61d923aa197946bae8
NUC12 <wjracr@outlook.com>
2022-10-11 15:26:42 +0800

    V0.1.0

--- CALLBACK_WindowProc.cpp
+++ CALLBACK_WindowProc.cpp
@@ -8,8 +8,10 @@
 #include <thread>
 using namespace std;
 
-void send_req();
-thread network;
+DWORD WINAPI send_req(LPVOID lpParam);
+bool ThreadStart = false;
+DWORD dwThread;
+HANDLE hThread;
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
@@ -23,13 +25,30 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
                 {
                     //network = thread(send_req);
                     //ShowWindow(hwnd, SW_HIDE);
+                    if (!ThreadStart)
+                    {
+                        CreateThread(
+                            NULL,                   // default security attributes
+                            0,                      // use default stack size  
+                            send_req,       // thread function name
+                            NULL,          // argument to thread function 
+                            0,                      // use default creation flags 
+                            &dwThread);
+                        Exit_Thread = false;
+                        ThreadStart = true;
+                    }
+                    else
+                    {
+                        MessageBox(hwnd, L"线程已启动", L"注意", MB_OK);
+                    }
                     return 0;
                 }
                 case BUT_STOP: 
                 {
                     Exit_Thread = true;
-                    //network.join();
-                    PostQuitMessage(0);
+                    WaitForMultipleObjects(1, &hThread, TRUE, INFINITE);
+                    CloseHandle(hThread);
+                    ThreadStart = false;
                     return 0;
                 }
             }
@@ -37,7 +56,8 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
         case WM_DESTROY:
         {
             Exit_Thread = true;
-            network.join();
+            WaitForMultipleObjects(1, &hThread, TRUE, INFINITE);
+            CloseHandle(hThread);
             PostQuitMessage(0);
             return 0;
         }
